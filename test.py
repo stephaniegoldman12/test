ce3576997e26326a953c8e105ff893ba58d2b850
Stephanie Goldman <stephaniegoldman4@gmail.com>
2023-09-18 17:53:27 -0500

    save

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ model = YOLO('yolov8n.pt')  # load an official model
 # model = YOLO('path/to/best.pt')  # load a custom trained
 
 # Export the model
-model.export(format='saved_model', export_dir='yolov8n_saved_model')
+model.export(format='saved_model')
